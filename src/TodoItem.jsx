15a25de3f5ae43769d6c8c108e171f8f88a9f7a4
andiedoescode <pang.andrea@gmail.com>
2024-01-10 10:16:24 -0500

    Added complete and delete function

---
+++ src/TodoItem.jsx
@@ -0,0 +1,39 @@
+import ListItem from "@mui/material/ListItem"
+import ListItemButton from "@mui/material/ListItemButton"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import ListItemText from "@mui/material/ListItemText"
+import Checkbox from "@mui/material/Checkbox"
+import IconButton from "@mui/material/IconButton"
+import CommentIcon from "@mui/icons-material/Comment"
+
+export default function TodoItem({ task, toggleItem, removeItem }) {
+	const labelId = `checkbox-list-label-${task.id}`
+
+	return (
+		<ListItem
+			key={task.id}
+			secondaryAction={
+				<IconButton edge='end' aria-label='comments' onClick={removeItem}>
+					<CommentIcon />
+				</IconButton>
+			}
+			disablePadding>
+			<ListItemButton
+				role={undefined}
+				// onClick={handleToggle(value)}
+				dense>
+				<ListItemIcon>
+					<Checkbox
+						edge='start'
+						checked={task.completed}
+						tabIndex={-1}
+						disableRipple
+						inputProps={{ "aria-labelledby": labelId }}
+						onChange={toggleItem}
+					/>
+				</ListItemIcon>
+				<ListItemText id={labelId} primary={task.text} />
+			</ListItemButton>
+		</ListItem>
+	)
+}
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,12 +1,7 @@
 import { Box, Typography } from "@mui/material"
 import List from "@mui/material/List"
-import ListItem from "@mui/material/ListItem"
-import ListItemButton from "@mui/material/ListItemButton"
-import ListItemIcon from "@mui/material/ListItemIcon"
-import ListItemText from "@mui/material/ListItemText"
-import Checkbox from "@mui/material/Checkbox"
-import IconButton from "@mui/material/IconButton"
-import CommentIcon from "@mui/icons-material/Comment"
+import TodoItem from "./TodoItem"
+
 import { useState } from "react"
 
 const tempTodos = [
@@ -18,46 +13,45 @@ const tempTodos = [
 export default function TodoList() {
 	const [todos, setTodos] = useState(tempTodos)
 
+	const toggleItem = (id) => {
+		setTodos((prevTodos) => {
+			return prevTodos.map((task) => {
+				if (task.id === id) {
+					return { ...task, completed: !task.completed }
+				} else {
+					return task
+				}
+			})
+		})
+	}
+
+	const removeItem = (id) => {
+		setTodos((prevTodos) => {
+			return prevTodos.filter((task) => task.id !== id)
+		})
+	}
+
 	return (
 		<>
-			{/* <Box sx={{ width: "100%", maxWidth: 500 }}> */}
-			<Typography variant='h3' component='h1' gutterBottom>
-				Pomo Focus
-			</Typography>
+			<Box sx={{ width: "100%", maxWidth: 500 }}>
+				<Typography variant='h3' component='h1' gutterBottom>
+					Pomo Focus
+				</Typography>
 
-			<List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-				{todos.map((task) => {
-					const labelId = `checkbox-list-label-${task.id}`
-
-					return (
-						<ListItem
-							key={task.id}
-							secondaryAction={
-								<IconButton edge='end' aria-label='comments'>
-									<CommentIcon />
-								</IconButton>
-							}
-							disablePadding>
-							<ListItemButton
-								role={undefined}
-								// onClick={handleToggle(value)}
-								dense>
-								<ListItemIcon>
-									<Checkbox
-										edge='start'
-										checked={task.completed}
-										tabIndex={-1}
-										disableRipple
-										inputProps={{ "aria-labelledby": labelId }}
-									/>
-								</ListItemIcon>
-								<ListItemText id={labelId} primary={task.text} />
-							</ListItemButton>
-						</ListItem>
-					)
-				})}
-			</List>
-			{/* </Box> */}
+				<List
+					sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+					{todos.map((task) => {
+						return (
+							<TodoItem
+								task={task}
+								key={task.id}
+								toggleItem={() => toggleItem(task.id)}
+								removeItem={() => removeItem(task.id)}
+							/>
+						)
+					})}
+				</List>
+			</Box>
 		</>
 	)
 }
